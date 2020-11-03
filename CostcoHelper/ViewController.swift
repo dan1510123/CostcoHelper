57eed803d7562e3f89388da1dd4c198010b9f30f
Daniel Luo <dyl24@case.edu>
2020-11-02 20:45:27 -0500

    Initial Commit

---
+++ CostcoHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CostcoHelper
+//
+//  Created by Daniel Luo on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
