90104c8675804951a8039178c2dd78a625732757
Daniel Luo <dyl24@case.edu>
2020-11-02 21:15:41 -0500

    Added photo gallery integration

--- CostcoHelper/ViewController.swift
+++ CostcoHelper/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        stopAnimating()
+    }
+    
+    private func startAnimating() {
+        self.activityIndicator.startAnimating()
+    }
+    
+    private func stopAnimating() {
+        self.activityIndicator.stopAnimating()
     }
 
-
+    @IBAction func touchUpInsideCameraButton(_ sender: Any) {
+        setUpGallery()
+    }
+    
+    private func setUpGallery() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePhotoLibraryPicker = UIImagePickerController()
+            imagePhotoLibraryPicker.delegate = self
+            imagePhotoLibraryPicker.allowsEditing = true
+            imagePhotoLibraryPicker.sourceType = .photoLibrary
+            self.present(imagePhotoLibraryPicker, animated: true, completion: nil)
+        }
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        self.imageView.image = image
+    }
+}
